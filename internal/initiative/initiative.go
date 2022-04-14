3972c588baae643d2bc5ebf6c78548a32532311e
David Taylor <dmtaylor2011@gmail.com>
2022-04-13 22:31:18 -0700

    wip more work here

--- internal/initiative/initiative.go
+++ internal/initiative/initiative.go
@@ -12,14 +12,18 @@ type InitiativeTracker struct {
 	pool *pgxpool.Pool
 }
 
-type initOrder struct {
+type InitOrder struct {
+	Id             uint64
+	OwnerSnowflake uint64
+	Size           uint
+	CurrentInOrder uint
 }
 
-type initUser struct {
-	id            uint64
-	initiativeId  uint64
-	userSnowflake uint64
-	userOrder     uint
+type InitUser struct {
+	Id            uint64
+	InitiativeId  uint64
+	UserSnowflake uint64
+	UserOrder     uint
 }
 
 func NewInitiativeTracker(pool *pgxpool.Pool) InitiativeTracker {
@@ -44,16 +48,16 @@ func (i InitiativeTracker) StartInit(owner uint64, participants []uint64) (uint6
 	defer tx.Rollback(ctx)
 	query := `INSERT INTO initiative_orders(owner_snowflake, size) VALUES ($1, $2) RETURNING id`
 	var init_id uint64
-	err = tx.QueryRow(ctx, query, owner, len(participants)).Scan(&init_id)
+	err = tx.QueryRow(ctx, query, owner, len(participants)-1).Scan(&init_id)
 	if err != nil {
 		return 0, errors.Wrap(err, "failed to create order")
 	}
-	users := make([]initUser, len(participants))
+	users := make([]InitUser, len(participants))
 	for idx, userSnowflake := range participants {
-		users[idx] = initUser{
-			initiativeId:  init_id,
-			userSnowflake: userSnowflake,
-			userOrder:     uint(idx),
+		users[idx] = InitUser{
+			InitiativeId:  init_id,
+			UserSnowflake: userSnowflake,
+			UserOrder:     uint(idx),
 		}
 	}
 
@@ -62,7 +66,7 @@ func (i InitiativeTracker) StartInit(owner uint64, participants []uint64) (uint6
 		pgx.Identifier{"initiative_users"},
 		[]string{"initiativeId", "user_snowflake", "user_order"},
 		pgx.CopyFromSlice(len(users), func(i int) ([]interface{}, error) {
-			return []interface{}{users[i].initiativeId, users[i].userSnowflake, users[i].userOrder}, nil
+			return []interface{}{users[i].InitiativeId, users[i].UserSnowflake, users[i].UserOrder}, nil
 		}),
 	)
 	if err != nil {
@@ -79,3 +83,62 @@ func (i InitiativeTracker) StartInit(owner uint64, participants []uint64) (uint6
 
 	return init_id, nil
 }
+
+func (i InitiativeTracker) GetUserUp(ctx context.Context, initOrder InitOrder) (InitUser, error) {
+	conn, err := i.pool.Acquire(ctx)
+	if err != nil {
+		return InitUser{}, errors.Wrap(err, "failed to get connection")
+	}
+	defer conn.Release()
+	query := `
+	SELECT iu.id, iu.user_snowflake, iu.user_order
+	FROM initiative_users iu, initiative_orders io
+	WHERE iu.initiative_id = io.id AND iu.user_order = io.current_in_order
+	`
+	var userId uint64
+	var userSnowflake uint64
+	var orderPosition uint
+	err = conn.Conn().QueryRow(ctx, query).Scan(&userId, &userSnowflake, &orderPosition)
+	if err != nil {
+		return InitUser{}, errors.Wrap(err, "failed to get current user via query")
+	}
+
+	return InitUser{
+		userId,
+		initOrder.Id,
+		userSnowflake,
+		orderPosition,
+	}, nil
+}
+
+/** TODO determine if this is needed
+func (i InitiativeTracker) getMostRecentForOwner(ctx context.Context, owner uint64) (initOrder, error) {
+	conn, err := i.pool.Acquire(ctx)
+	if err != nil {
+		return initOrder{}, errors.Wrap(err, "failed to get connection")
+	}
+	defer conn.Release()
+
+	query := `
+	SELECT id, size, current_in_order
+	FROM initiative_orders
+	WHERE owner_snowflake = $1
+	ORDER BY id DESC
+	LIMIT 1
+	`
+	var id uint64
+	var size uint
+	var current uint
+	err = conn.Conn().QueryRow(ctx, query, owner).Scan(&id, &size, &current)
+	if err != nil {
+		return initOrder{}, errors.Wrap(err, "failed to get most recent order for user")
+	}
+
+	return initOrder{
+		id,
+		owner,
+		size,
+		current,
+	}, nil
+}
+*/

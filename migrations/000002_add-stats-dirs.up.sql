75d967db96d58140973227ff95eb8b1d153fcd6e
David Taylor <dmtaylor2011@gmail.com>
2023-01-11 22:26:33 -0800

    add index for months

--- migrations/000002_add-stats-dirs.up.sql
+++ migrations/000002_add-stats-dirs.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS discord_usage_stats (
     id SERIAL PRIMARY KEY,
     guild_id NUMERIC NOT NULL,
-    uid NUMERIC NOT NULL,
+    user_id NUMERIC NOT NULL,
     report_month VARCHAR(7) NOT NULL,
     message_count INTEGER DEFAULT 1
 );
 
 CREATE INDEX report_guild_users ON discord_usage_stats(guild_id, uid);
+CREATE INDEX report_guild_months ON discord_usage_stats(guild_id, report_month);

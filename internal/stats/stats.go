ba6409b3915c870b9b7eddfdb46e8c396740da2d
David Taylor <dmtaylor2011@gmail.com>
2023-01-14 23:18:26 -0800

    add stats package w/log activity

---
+++ internal/stats/stats.go
@@ -0,0 +1,72 @@
+package stats
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/pkg/errors"
+
+	"github.com/dmtaylor/costanza/internal/db"
+)
+
+type Stats struct {
+	pool *pgxpool.Pool
+}
+
+func New(pool *pgxpool.Pool) Stats {
+	return Stats{
+		pool,
+	}
+}
+
+func (s Stats) LogActivity(ctx context.Context, guildId, userId uint64, reportMonth string) error {
+	conn, err := s.pool.Acquire(ctx)
+	if err != nil {
+		return errors.Wrap(err, "failed to get pool connection")
+	}
+	defer conn.Release()
+
+	var existingLogId uint
+	err = conn.QueryRow(ctx, `
+SELECT id
+FROM discord_usage_stats
+WHERE guild_id = $1 AND user_id = $2 AND report_month = $3
+`, guildId, userId, reportMonth).Scan(existingLogId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			// No usage so far, insert new record
+			_, err = conn.Exec(ctx, "INSERT INTO discord_usage_stats(guild_id, user_id, report_month) VALUES ($1, $2, $3)",
+				guildId,
+				userId,
+				reportMonth)
+			if err != nil {
+				return errors.Wrap(err, "failed to insert new record")
+			}
+			return nil
+		} else {
+			// Error case
+			return errors.Wrap(err, "failed to get existing stat")
+		}
+	}
+	_, err = conn.Exec(ctx, `
+UPDATE discord_usage_stats
+SET message_count = message_count + 1
+WHERE id = $1
+`, existingLogId)
+	if err != nil {
+		return errors.Wrap(err, "failed to increment message stat")
+	}
+
+	return nil
+}
+
+func (s Stats) GetLeaders(ctx context.Context, guildId uint64, reportMonth string) ([]db.DiscordUsageStat, error) {
+	conn, err := s.pool.Acquire(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get pool connection")
+	}
+	defer conn.Release()
+	// TODO implement
+	return nil, nil
+}
--- migrations/000002_add-stats-dirs.up.sql
+++ migrations/000002_add-stats-dirs.up.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS discord_usage_stats (
     message_count INTEGER DEFAULT 1
 );
 
-CREATE INDEX report_guild_users ON discord_usage_stats(guild_id, uid);
+CREATE INDEX report_guild_users ON discord_usage_stats(guild_id, user_id);
 CREATE INDEX report_guild_months ON discord_usage_stats(guild_id, report_month);

960654d44df9576c61e41753bb99ff6012d12b2e
David Taylor <dmtaylor2011@gmail.com>
2024-01-08 14:56:20 -0800

    add migrations for reaction stats

---
+++ migrations/000006_reaction_stats.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX reaction_guild_months;
+DROP INDEX reaction_guild_users;
+
+DROP TABLE discord_reaction_stats;
---
+++ migrations/000006_reaction_stats.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS discord_reaction_stats (
+    id SERIAL PRIMARY KEY,
+    guild_id NUMERIC NOT NULL,
+    user_id NUMERIC NOT NULL,
+    report_month VARCHAR(7) NOT NULL,
+    message_count INTEGER NOT NULL DEFAULT 1
+);
+
+CREATE INDEX reaction_guild_users ON discord_reaction_stats(guild_id, user_id);
+CREATE INDEX reaction_guild_months ON discord_reaction_stats(guild_id, report_month);
